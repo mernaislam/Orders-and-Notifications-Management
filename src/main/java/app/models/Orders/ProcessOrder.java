ef920c86ce8eb137cca34980ae3b29cf25cf9dfd
Mohamed Waleed <mohamed.waleed1023@gmail.com>
2023-12-30 08:25:34 +0200

    ProcessOrder class - template method pattern
    Added assignOrderID(), randomly generates an id.
    validateOrder(), validates the order before placing.

--- src/main/java/app/models/Orders/ProcessOrder.java
+++ src/main/java/app/models/Orders/ProcessOrder.java
@@ -1,19 +1,33 @@
 package app.models.Orders;
 
+import app.models.Product.Product;
+import app.repos.OrderRepo;
+import app.repos.ProductRepo;
+import app.service.OrderService;
+import app.service.ProductService;
+
 public abstract class ProcessOrder {
+    protected OrderService orderService;
+    protected ProductRepo productRepo;
+    public ProcessOrder(OrderService orderService, ProductRepo productRepo) {
+        this.orderService = orderService;
+        this.productRepo = productRepo;
+    }
     public final void processOrder(Order order) {
         // Template method - Skeleton
+        assignOrderID(order);
         calculateProductFees(order);
         calculateShippingFees(order);
         calculateTotalFees(order);
-        updateOrderStatus(order);
+        validateOrder(order);
     }
+
+    public abstract void assignOrderID(Order order);
+
     public abstract void calculateProductFees(Order order);
     public abstract void calculateShippingFees(Order order);
+    public abstract void validateOrder(Order order);
     public final void calculateTotalFees(Order order) {
         order.setTotalPrice(order.productsFees + order.shippingFees);
-    }
-    public final void updateOrderStatus(Order order) {
-        order.setStatus(OrderStatus.PLACED);
     }
 }
