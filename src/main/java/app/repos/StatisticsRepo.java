d20622f7d6e11e74f9681a2679681fadcb503038
Nada Mohammed <naddaa26@gmail.com>
2023-12-31 20:47:00 +0200

    .

--- src/main/java/app/repos/StatisticsRepo.java
+++ src/main/java/app/repos/StatisticsRepo.java
@@ -1,13 +1,19 @@
 package app.repos;
 
 import app.models.Customer.Customer;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
+@Component
 public class StatisticsRepo {
     private HashMap<String, Integer> notificationTemplateCount = new HashMap<>();
     private HashMap<String, Integer> channelCount = new HashMap<>();
     private HashMap<Customer, Integer> customerNotificationCount = new HashMap<>();
+
+    public StatisticsRepo() {
+
+    }
 
     public HashMap<String, Integer> getNotificationTemplateCount() {
         return notificationTemplateCount;
